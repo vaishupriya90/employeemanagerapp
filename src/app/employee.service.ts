d61ad895277af1de56f68a0d8185eb3bce71c0d5
vaishu <vaishupriya90@gmail.com>
2021-03-26 00:48:44 -0500

    imported the employee service in the app component and created a method to getAllEmployees by accessing the employeeservice methods.subscribe(), store the data extracted in a local variable and render the local variable to the app.html

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Employee } from './employee'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private apiServerUrl : string = '';
+  private apiServerUrl : string = environment.apiBaseUrl;
 
   constructor(private http : HttpClient) { }
 
